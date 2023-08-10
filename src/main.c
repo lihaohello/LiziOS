7a8adcbc27dde3477eab011e9cd909a29f59fcae
lihaohello <lihaohello@126.com>
2023-08-10 19:40:29 +0800

    完成保护模式设置，进入C语言

--- src/main.c
+++ src/main.c
@@ -1,13 +1,11 @@
-char message[] = "hello kernel!!!";
+char message[] = "hello";
 
 int main() {
     char* video = (char*)0xb8000;
 
     for (int i = 0; i < sizeof(message); i++) {
         *video = message[i];
-        video++;
-        *video = 0b00100101;
-        video++;
+        video+=2;
     }
 
     return 0;
--- src/start.asm
+++ src/start.asm
@@ -5,6 +5,13 @@ extern main
 section .text
 global _start
 _start:
-    mov byte [0xb8000], 'S';
+    mov byte [0xb8000], 'S'
+    mov byte [0xb8000+2], 'B'
+
+    xchg bx,bx
+
     call main
+
+    xchg bx,bx
+
     jmp $
