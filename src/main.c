a76d2b7df19d135348cba108d11d72195d8e44ee
lihaohello <lihaohello@126.com>
2024-06-07 15:44:50 +0800

    实现单字符打印功能，书本上的程序有误

--- src/main.c
+++ src/main.c
@@ -1,5 +1,32 @@
+#include "print.h"
 int main(void) {
-    while (1) {
-    }
+    put_char('\n');
+    put_char('\n');
+    put_char('\n');
+    put_char('\n');
+    put_char('\n');
+    put_char('T');
+    put_char('h');
+    put_char('i');
+    put_char('s');
+    put_char(' ');
+    put_char('i');
+    put_char('s');
+    put_char(' ');
+    put_char('L');
+    put_char('i');
+    put_char('z');
+    put_char('i');
+    put_char('O');
+    put_char('S');
+    put_char('!');
+    put_char('\n');
+    put_char('D');
+    put_char('o');
+    put_char(' ');
+    put_char('i');
+    put_char('t');
+    put_char('!');
+
     return 0;
 }
