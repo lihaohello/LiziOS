c4b6bec6abb83fd888d5289c99b51648dc41a3b6
lihaohello <lihaohello@126.com>
2024-06-25 05:38:38 -0700

    feat(qemu): Add qemu debug

--- src/main.c
+++ src/main.c
@@ -7,6 +7,9 @@ int main(void) {
     print_str("Let\'s enjoy the journey!\n");
 
     init_all();
+
+    // 开始切换保护模式之前，关闭中断
+    // 中断描述符加载完整后，才可以开启中断
     asm volatile("sti");
     while (1) {
     };
